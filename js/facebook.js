a863d910993f46cffe295fe238e8f6dea3c150ea
Alex-Tsvetanov <alex_tsvetanov_2002@abv.bg>
2017-05-01 16:30:57 +0300

    first commit

---
+++ js/facebook.js
@@ -0,0 +1,29 @@
+function checkLoginState() {
+	FB.getLoginStatus(function(response) {
+		console.log(response);
+		if (response.status === 'connected') {
+			FB.api('/me?fields=id,name,email,permissions', function(responseAPI) {
+			});
+		} else {
+			document.getElementById('status').innerHTML = 'Please log into this app.';
+		}
+	});
+}
+
+function init () {
+	FB.Event.subscribe('auth.authResponseChange', function(response) {
+		if (response.status === 'connected') {
+			FB.api('/me', function(response) {
+				console.log(response);
+			});   
+		} else if (response.status === 'not_authorized') {
+			FB.login(function(response) {}, {scope: 'email'});
+		} else {
+			FB.login(function(response) {}, {scope: 'email'});
+		}
+	});
+	checkLoginState();
+}
+
+window.document.onload = function() { init(); };
+window.onload = function() { init(); };
